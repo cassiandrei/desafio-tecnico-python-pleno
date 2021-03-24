87ed66c51f20581ddc82ed469919340272eed938
Cassiano Andrei <cschneider@inf.ufsm.br>
2021-03-24 20:22:18 -0300

    Meus favoritos

--- api/core/urls.py
+++ api/core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from core.views import index, addheroi, favorite
+from core.views import index, addheroi, favorite, my_favorites
 
 app_name = 'core'
 urlpatterns = [
     path('', index, name='index'),
     path('addheroi/', addheroi, name='addheroi'),
     path('favoritar/', favorite, name='favoritar'),
+    path('favoritos/', my_favorites, name='meus_favoritos'),
 ]
