4b1c82e2e2196502004c3d040a462fdcc5bbf3e6
Cassiano Andrei Dias Da Silveira Schneider <cschneider@inf.ufsm.br>
2021-03-25 05:45:21 -0300

    Delete update.sh

--- update.sh
+++
@@ -1,4 +0,0 @@
-sudo cp -r api/* /var/lib/docker/volumes/delivery_api_data/_data
-docker exec -i -t api-delivery pip install -r requirements.txt
-docker exec -i -t api-delivery python manage.py makemigrations
-docker exec -i -t api-delivery python manage.py migrate
